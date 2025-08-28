2180592e0e663927e1844de732c002319b9bb5b7
Cristian Gonzalez <cristian.glez.m@gmail.com>
2025-08-28 15:21:32 +0200

    Migrated to sfml 3

--- src/NbodySim.hpp
+++ src/NbodySim.hpp
@@ -1,9 +1,9 @@
 ////////////////////////////////////////////////////////////
-// Copyright 2014-2019 Cristian Glez <Cristian.glez.m@gmail.com>
+// Copyright 2014-2025 Cristian Glez <Cristian.glez.m@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may not a copy of the License at
 //
 //  http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -30,32 +30,32 @@ class NbodySim{
     public:
         NbodySim(std::size_t numParticles);
         void run() noexcept;
-        void handleInput(sf::Event& e) noexcept;
+        void handleInput(const sf::Event& e) noexcept;
         void update(sf::Time dt) noexcept;
         void updateFPS(sf::Time dt) noexcept;
         void render() noexcept;
         sf::FloatRect updateBounds(const sf::View& view) noexcept;
         ~NbodySim() = default;
     private:
+        sf::RenderWindow win;
+        sf::FloatRect bounds;
+        std::vector<Particle> particles;
+        BarnesHutTree bht;
         float mass;
+        sf::Font FPSFont;
+        sf::Text FPSText;
+        sf::Text sizeText;
+        sf::Text massText;
         bool fastGen;
         bool renderQuadtree;
         bool blackHole;
-        sf::RenderWindow win;
-        sf::FloatRect bounds;
-        BarnesHutTree bht;
-        std::vector<Particle> particles;
         static std::mt19937 engine;
         std::uniform_real_distribution<> diceForX;
         std::uniform_real_distribution<> diceForY;
         std::uniform_real_distribution<> diceForMass;
         static const sf::Time TimePerFrame;
-        sf::Text FPSText;
-        sf::Text sizeText;
         sf::Time FPSUpdateTime;
         std::size_t FPSNumFrames;
-        sf::Font FPSFont;
-        sf::Text massText;
 };
 
 #endif // NBODYSIM_HPP
